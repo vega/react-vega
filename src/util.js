340a05263a6212d6b9bc44f4f808adab4dae7ec0
Krist Wongsuphasawat <krist.wongz@gmail.com>
2016-08-05 23:03:56 -0700

    Update 2016-08-06T06:03:55.442Z

--- src/util.js
+++
@@ -1,8 +0,0 @@
-export function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-export function isFunction(functionToCheck) {
-  const getType = {};
-  return !!functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-}
