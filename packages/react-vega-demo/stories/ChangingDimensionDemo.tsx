1846cd30c313ccc8b4a3679a6ab95f453eb85028
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-02-05 02:10:20 -0800

    docs: add storybook demo

---
+++ packages/react-vega-demo/stories/ChangingDimensionDemo.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { VegaLite } from "../../react-vega/src";
+
+export default class ChangingDimensionDemo extends React.Component<{}, {
+  width: number;
+  height: number;
+  padding: number | {
+    top?: number;
+    bottom?: number;
+    left?: number;
+    right?: number;
+  };
+  grow: boolean;
+}> {
+  interval: NodeJS.Timeout;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 100,
+      height: 100,
+      padding: 20,
+      grow: true,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(({width, height, padding, grow}) => ({
+        width: width + (grow ? 1 : -1),
+        height: height + (grow ? 1 : -1),
+        grow: grow && width < 400 || !grow && width === 100
+      }));
+    }, 10);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { width, height, padding } = this.state;
+
+    const SPEC = {
+      $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+      width,
+      height,
+      padding,
+      data: {
+        values: [
+          { a: "A", b: 28 },
+          { a: "B", b: 55 },
+          { a: "C", b: 43 },
+          { a: "D", b: 91 },
+          { a: "E", b: 81 },
+          { a: "F", b: 53 },
+          { a: "G", b: 19 },
+          { a: "H", b: 87 },
+          { a: "I", b: 52 }
+        ],
+        name: "source"
+      },
+      selection: {
+        a: { type: "single" }
+      },
+      mark: "bar",
+      encoding: {
+        x: { field: "a", type: "ordinal" },
+        y: { field: "b", type: "quantitative" },
+        tooltip: { field: "b", type: "quantitative" },
+        color: {
+          condition: { selection: "a", value: "steelblue" },
+          value: "grey"
+        }
+      }
+    } as const;
+
+    return <VegaLite spec={SPEC} />;
+  }
+}
