5b6d086544b197c0abe17681297c5aeac6ae1587
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-03-22 16:56:56 -0700

    test: add more unit tests

---
+++ packages/react-vega/test/createClassFromSpec.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { createClassFromSpec } from '../src';
+import spec from './mock/vegaLiteSpec';
+
+describe('createClassFromSpec', () => {
+  const Component = createClassFromSpec({ mode: 'vega-lite', spec });
+
+  describe('.getSpec()', () => {
+    it('returns spec', () => {
+      expect(Component.getSpec()).toBe(spec);
+    });
+  });
+
+  it('renders', () => {
+    const wrapper = mount(<Component />);
+
+    return new Promise(done => {
+      setTimeout(() => {
+        const renderedWrapper = wrapper.render();
+        expect(renderedWrapper.find('svg')).toHaveLength(1);
+      });
+      done();
+    });
+  });
+});
