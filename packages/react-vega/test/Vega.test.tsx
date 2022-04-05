3b0512f61ef6856869424697dac6d880503b4939
krist-wongsuphasawat <krist.wongsuphasawat@airbnb.com>
2022-04-05 10:56:11 -0700

    test: fix unit tests

--- packages/react-vega/test/Vega.test.tsx
+++ packages/react-vega/test/Vega.test.tsx
@@ -1,65 +1,60 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Vega } from '../src';
 import spec from './mock/vegaLiteSpec';
 
 describe('<Vega>', () => {
-  it('renders', () => {
-    render(<Vega mode="vega-lite" spec={spec} />);
-    expect(screen.getByRole('svg')).toHaveLength(1);
+  const data1 = {
+    source: [
+      { a: 'B', b: 29 },
+      { a: 'C', b: 20 },
+    ],
+  };
+  const data2 = {
+    source: [
+      { a: 'B', b: 29 },
+      { a: 'C', b: 20 },
+      { a: 'D', b: 15 },
+    ],
+  };
+
+  it('renders', async () => {
+    const { container } = render(<Vega mode="vega-lite" spec={spec} renderer="svg" />);
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+    expect(container.querySelector('svg')).toBeDefined();
+    expect(container.querySelectorAll('g.mark-rect path')).toHaveLength(5);
   });
 
-  it('renders with data', () => {
-    render(
-      <Vega
-        mode="vega-lite"
-        spec={spec}
-        data={{
-          source: [{ category: 'A', amount: 28 }],
-        }}
-      />,
-    );
+  it('renders with data', async () => {
+    const { container } = render(<Vega mode="vega-lite" renderer="svg" spec={spec} data={data1} />);
 
-    expect(screen.getByRole('svg')).toHaveLength(1);
+    await new Promise(resolve => setTimeout(resolve, 0));
+    expect(container.querySelector('svg')).toBeDefined();
+    expect(container.querySelectorAll('g.mark-rect path')).toHaveLength(2);
   });
 
-  it('updates data if changed', () => {
-    const { rerender } = render(
-      <Vega
-        mode="vega-lite"
-        spec={spec}
-        data={{
-          source: [{ a: 'A', b: 28 }],
-        }}
-      />,
+  it('updates data if changed', async () => {
+    const { container, rerender } = render(
+      <Vega mode="vega-lite" renderer="svg" spec={spec} data={data1} />,
     );
 
-    expect(screen.getByRole('rect')).toHaveLength(1);
+    await new Promise(resolve => setTimeout(resolve, 0));
+    expect(container.querySelectorAll('g.mark-rect path')).toHaveLength(2);
 
-    rerender(
-      <Vega
-        mode="vega-lite"
-        spec={spec}
-        data={{
-          data: {
-            source: [
-              { a: 'B', b: 29 },
-              { a: 'C', b: 20 },
-            ],
-          },
-        }}
-      />,
-    );
+    rerender(<Vega mode="vega-lite" renderer="svg" spec={spec} data={data2} />);
 
-    expect(screen.getByRole('rect')).toHaveLength(2);
+    await new Promise(resolve => setTimeout(resolve, 10));
+    expect(container.querySelectorAll('g.mark-rect path')).toHaveLength(3);
   });
 
-  it('does not update data if does not changed', () => {
-    const source = [{ a: 'A', b: 28 }];
+  it('does not update data if does not changed', async () => {
+    const { container, rerender } = render(
+      <Vega mode="vega-lite" renderer="svg" spec={spec} data={data1} />,
+    );
+    rerender(<Vega mode="vega-lite" renderer="svg" spec={spec} data={data1} />);
 
-    const { rerender } = render(<Vega mode="vega-lite" spec={spec} data={{ source }} />);
-    rerender(<Vega mode="vega-lite" spec={spec} data={{ source }} />);
-
-    expect(screen.getByRole('rect')).toHaveLength(1);
+    await new Promise(resolve => setTimeout(resolve, 0));
+    expect(container.querySelectorAll('g.mark-rect path')).toHaveLength(2);
   });
 });
--- packages/react-vega/test/VegaLite.test.tsx
+++ packages/react-vega/test/VegaLite.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { VegaLite } from '../src';
 import spec from './mock/vegaLiteSpec';
 
-describe('<Vega>', () => {
-  it('renders', () => {
-    render(<VegaLite spec={spec} />);
-    expect(screen.getByRole('svg')).toHaveLength(1);
+describe('<VegaLite>', () => {
+  it('renders', async () => {
+    const { container } = render(<VegaLite spec={spec} renderer="svg" />);
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+    expect(container.querySelector('svg')).toBeDefined();
+    expect(container.querySelectorAll('g.mark-rect path')).toHaveLength(5);
   });
 });
